36837b0dc4d7e867a55ea0e130b60336b5a4293c
Seth Silesky <seth.silesky@gmail.com>
2018-09-29 15:15:42 -0500

    primitive implementation working

--- client/app.js
+++ client/app.js
@@ -1,1 +1,25 @@
-console.log('sup!')
+const ESTIMATION_INPUT_ID = 'estimation';
+const SUBMIT_BUTTON_ID = 'submit';
+const WS_URL = 'ws://localhost:3333/ws';
+
+const socket = new WebSocket(WS_URL);
+
+socket.addEventListener('open', function() {
+  console.log('Websocket opened @ ' + WS_URL);
+});
+
+socket.addEventListener('message', function(event) {
+  console.log('Message from server ', event.data);
+});
+
+const createMessage = (username, estimate) => {
+  const est = typeof estimate === 'string' ? parseInt(estimate, 10) : estimate;
+  return JSON.stringify({ username, estimate: est });
+};
+
+document.getElementById(SUBMIT_BUTTON_ID).addEventListener('click', () => {
+  const value = document.getElementById(ESTIMATION_INPUT_ID).value;
+  const msg = createMessage('Seth', value);
+  console.log('msg submitted:', msg);
+  socket.send(msg);
+});
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"estimate/models"
 	"fmt"
 	"log"
@@ -10,13 +11,58 @@ import (
 )
 
 // globals
-var clients = make(map[*websocket.Conn]bool)
+var clients = make(map[*websocket.Conn]bool) // create in-memory global map to keep track of client data
+
 var broadcast = make(chan models.Estimation)
 var upgrader = websocket.Upgrader{}
 
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// upgrade initial GET to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clients[ws] = true
+
+	for {
+		var msg models.Estimation
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			if e, ok := err.(*json.SyntaxError); ok {
+				log.Printf("syntax error at byte offset %d", e.Offset)
+			}
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+
+	defer ws.Close()
+}
+
+// goroutine that runs in a new thread
+func handleEstimations() {
+	for {
+		// grab the next msg from the broadcast chanell
+		msg := <-broadcast
+		log.Printf("msg", msg)
+		// iterate over each client
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
 func main() {
 	handler := http.FileServer(http.Dir("./client"))
 	http.Handle("/", handler)
+	http.HandleFunc("/ws", handleConnections)
+	go handleEstimations()
 	fmt.Println("Listing on 3333.")
 	log.Fatal(http.ListenAndServe(":3333", nil))
 }
